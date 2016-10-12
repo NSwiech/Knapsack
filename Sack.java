1900b9e448a8da616dd4a84e5e24d602e59cf8d6
Nicolas Swiech <nicolas.swiech@gmail.com>
2016-10-12 16:59:01 +0200

    Update Sack.java

--- Sack.java
+++ Sack.java
@@ -4,29 +4,29 @@ import java.util.Random;
 
 public class Sack {
 	private int sackID;
-	private int capacity=0;
+	private int capacity = 0;
 	private Random randomGenerator = new Random();
-	private ArrayList contence = new ArrayList();
-	
-	public Sack(){
-		capacity = 15 + randomGenerator.nextInt(10); 
+	private ArrayList<Item> contence = new ArrayList<Item>();
+
+	public Sack() {
+		capacity = 15 + randomGenerator.nextInt(10);
 	}
-	
-public int getWeight(){
-	int weight;
-	for(Item x : contence){
-		weight +=Item.getWeight();
+
+	public int getWeight() {
+		int weight = 0;
+		for (Item x : contence) {
+			weight += x.getWeight();
+		}
+		return weight;
 	}
-	return weight;
-}
 
 public boolean addItem(Item itm){
 	boolean addOK = false;
 	
-	if((capacity) - getweight()) <= itm.weight()){
+	if((capacity) - getWeight() <= itm.getWeight()){
 		contence.add(itm);
 		addOK = true;
 	}
-	return addOk;
+	return addOK;
 }
 }
